05004421126d64591905db77e7697fcb53373f5b
Anjar Oki Wibowo <anjar.les.son@gmail.com>
2024-01-19 11:18:29 +0700

    add lesson 01192024

---
+++ latihan5/pesawat.js
@@ -0,0 +1,13 @@
+var no_pesawat = 1;
+var kondisi = "";
+
+while (no_pesawat <= 20) {
+  kondisi = confirm("Apakah konisi pesawat baik ?");
+  if (kondisi == true) {
+    var hkondisi = "baik";
+  } else {
+    var hkondisi = "buruk";
+  }
+  console.log("No. Pesawat" + no_pesawat + " dalam keadaan " + hkondisi);
+  no_pesawat++;
+}
