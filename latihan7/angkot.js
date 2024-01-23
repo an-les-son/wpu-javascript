79693c0fa43b64cf791a2a5d59ddbf9674a8550f
Anjar O <anjar.oki@gmail.com>
2024-01-23 10:59:49 +0700

    add latihan 7 & 8

--- latihan7/angkot.js
+++ latihan7/angkot.js
@@ -5,8 +5,9 @@ for (var noAngkot = 1; noAngkot <= jmlhAngkot; noAngkot++) {
         console.log("Angkot No. "+noAngkot+" beroperasi dengan baik.")
     } else if ( noAngkot === 8 ) {
         console.log("Angkot No. "+noAngkot+" sedang lembur.")
+    } else if ( noAngkot === 10 ) {
+        console.log("Angkot No. "+noAngkot+" sedang lembur.")
     } else {
         console.log("Angkot No. "+noAngkot+" sedang tidak beroperasi.")
     }
-    
 }
--- latihan8/angkot.js
+++ latihan8/angkot.js
@@ -1,12 +1,13 @@
 var jmlhAngkot = 10;
 
 for (var noAngkot = 1; noAngkot <= jmlhAngkot; noAngkot++) {
-    if (noAngkot <= 6) {
+    if (noAngkot === 5) {
+        console.log("Angkot No. "+noAngkot+" sedang lembur.")
+    } else if (noAngkot <= 6) {
         console.log("Angkot No. "+noAngkot+" beroperasi dengan baik.")
-    } else if ( noAngkot === 8 ) {
+    } else if ( noAngkot === 8 || noAngkot === 10) {
         console.log("Angkot No. "+noAngkot+" sedang lembur.")
     } else {
         console.log("Angkot No. "+noAngkot+" sedang tidak beroperasi.")
     }
-    
 }
